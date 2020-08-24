f6ad10501f7cc81ff4d69d1e503b51babe466e97
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2020-08-24 20:25:50 +0200

    changes

--- Django/Nail_Budapest/meetings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    # Home page
-    path('', views.home, name='home'),
-    path('appointment', views.meeting, name='appointment'),
-    path('detail/<int:id>', views.detail, name='detail'),
-    path('new_app', views.new_app, name='new_app'),
-]
