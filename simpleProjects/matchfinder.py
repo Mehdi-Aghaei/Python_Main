894c8ad73190e809092e13a05744ab6f43e87a63
Cacti79 <mehdi1020@yahoo.com>
2020-12-01 00:12:56 +0100

    last change

--- simpleProjects/matchfinder.py
+++ simpleProjects/matchfinder.py
@@ -10,23 +10,3 @@ def matchFinder(list1,list2):
 a = [1,2,3]
 b = [3,2,4]
 print(matchFinder(a,b))
-# Program to check if a Python list contains elements of another list
-'''
-def list_contains(List1, List2): 
-
-    set1 = set(List1) 
-    set2 = set(List2) 
-    if set1.intersection(set2): 
-        return True 
-    else: 
-        return False
-
-# Test Case 1
-List1 = ['a', 'e', 'i', 'o', 'u'] 
-List2 = ['x', 'y', 'z', 'l', 'm'] 
-print("Test Case#1 ", list_contains(List1, List2)) 
-
-# Test Case 2  
-List1 = ['a', 'e', 'i', 'o', 'u']  
-List2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']  
-print("Test Case#2 ", list_contains(List1, List2)) '''
